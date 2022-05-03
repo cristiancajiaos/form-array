e5a4cb745f479ba3049f9c3cf6dc52e3a43b6800
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-03 15:09:52 -0400

    Definido layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  courses: FormArray;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.courses = new FormArray([]);
+
+    this.form = this.fb.group({
+      courses: this.courses
+    });
+  }
+
+  addCourse() {
+    this.courses.push(new FormControl())
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
   }
 
 }
